29b9975879600925908cb1d44d36b2e1e4a1702f
Ghost-dev-netizen <ghostscurity6@gmail.com>
2024-07-23 20:23:50 -0300

    Subiendo modificaciones

--- Control de Reparaciones.py
+++ Control de Reparaciones.py
@@ -1,16 +1,42 @@
 import tkinter as tk 
-windows = tk.Tk() 
-windows.title('Control de reparaciones')
-windows.geometry("800x900")
-windows.minsize(600, 100)
-windows.maxsize(800, 600)
-windows.iconbitmap("imgfactura.ico")
-windows.configure(bg="seagreen3")
-windows.resizable(False,True)
+root = tk.Tk() 
+root.title('Control de reparaciones')
+root.geometry("800x900")
+root.minsize(600, 100)
+root.maxsize(800, 600)
+root.iconbitmap("imgfactura.ico")
+root.configure(bg="seagreen3")
+root.resizable(False,True)
 
-framewindows = tk.frame(windows)
+#area de menu
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+
+filemenu = tk.Menu(menubar, tearoff=0)
+editmenu = tk.Menu(menubar, tearoff=0)
+helpmenu = tk.Menu(menubar, tearoff=0)
+
+filemenu.add_command(label="Nuevo")
+filemenu.add_command(label="Abrir")
+filemenu.add_command(label="Guardar")
+filemenu.add_command(label="Cerrar")
+filemenu.add_separator()
+filemenu.add_command(label="Salir", command=root.quit)
 
 
-framewindows.pack()
+editmenu.add_command(label="Cortar")
+editmenu.add_command(label="Copiar")
+editmenu.add_command(label="Pegar")
 
-windows.mainloop()
+
+helpmenu.add_command(label="Ayuda")
+helpmenu.add_separator()
+helpmenu.add_command(label="Acerca de...")
+
+menubar.add_cascade(label="Archivo", menu=filemenu)
+menubar.add_cascade(label="Editar", menu=editmenu)
+menubar.add_cascade(label="Ayuda", menu=helpmenu)
+
+
+
+root.mainloop()
